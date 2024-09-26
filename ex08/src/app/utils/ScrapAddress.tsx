3e75ce20268822f1c64111d5699c68b83ba565ad
Juiceee <lbehr@student.42.fr>
2024-09-26 15:42:49 +0200

    finish ex08

--- ex08/src/app/utils/ScrapAddress.tsx
+++ ex08/src/app/utils/ScrapAddress.tsx
@@ -2,7 +2,7 @@
 import fs from 'fs';
 
 export default async function ScrapAddress(): Promise<string[]> {
-	const data = fs.readFileSync('./addresscopy.txt',
+	const data = fs.readFileSync('./address.txt',
 		{ encoding: 'utf8', flag: 'r' }).split('\n').map(address => address.replace('\r', '').trim()).filter(address => address !== '');;
 
 	return (data);
